98ff92761a562e2c92facd32c8b144ae729a852d
Greg Woodside <greg.woodside@backstopsolutions.com>
2024-02-18 19:06:33 -0600

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Dashboard from "./components/Dashboard";
 import { formattedDate } from "./components/CurrentTime";
 import BirthdayList from "./components/BirthdayList";
 import BirthdayForm from "./components/BirthdayForm";
-import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/SignUp";
 import AuthDetails from "./components/auth/AuthDetails";
 import {
   addDoc,
